03efe38e244ceb1dec9202002ccaf35f2acd868e
LRosen656 <rosen656@umn.edu>
2023-02-21 16:47:02 -0600

    Testing Geojson.

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Yep!"
+    return "This website is working!"
 
 
-@app.route("/hello")
-def hello():
-    return "hello"
+@app.route("/ffc")
+def get_polygon():
+  conn = psycopg2.connect(host = '34.171.19.177', database = 'lab1', user = 'postgres', password = 'starman1')
+  cursor = conn.cursor()
+  cursor.execute("SELECT ST_AsGeoJSON(geom) FROM table_poly;")
+  result = cursor.fetchall()
+  conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
