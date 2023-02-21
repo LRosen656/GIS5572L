e7aef21848aaab0fd070363cb2d671e308a48975
LRosen656 <rosen656@umn.edu>
2023-02-21 17:14:16 -0600

    Attempted featureclass

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def hello_world():
 def get_polygon():
   conn = psycopg2.connect(host = '34.171.19.177', database = 'lab1', user = 'postgres', password = 'starman1')
   cursor = conn.cursor()
-  cursor.execute("SELECT ST_AsGeoJSON(geom) FROM table_poly;")
+  cursor.execute("SELECT json_build_object('type',       'Feature', 'id',         gid, 'geometry',   ST_AsGeoJSON(geom)::json, 'properties', json_build_object('feat_type', feat_type,'feat_area', ST_Area(geom)::geography))FROM table_poly;;")
   result = cursor.fetchall()
   conn.close()
   return result[0][0]
